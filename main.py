32d66dae67012ce30525c57e634d56395b99d193
RuslanaPashkutska <ryslana.pashkytska@gmail.com>
2025-02-28 23:45:15 +0100

    colorama ouput

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from datetime import datetime
 from id_generator import id_generator
+
+from printer_functions import print_error, print_success, input_colored
+
+from colorama import init
+
+
+
 '''
 
 Додаток, який буде зберігати нотатки
@@ -25,8 +32,9 @@ if note_date_one ["creation_date"] > note_date_one ["creation_date"]:
 4) Написати цикл, який буде отримувати інформацію від користувача та реагувати на неї
 
 5) Пофіксити проблему де в нас є глобальна змінна
-6) 
+6) Пофіксити випадок,коли ми не передаємо аргументи print_note
 '''
+init(autoreset=True)
 
 note_list = [] # {"creation_date": "13.02.2025 21:03", "text": "This is my note, that I am taking on my laptop", id: 1}
 note_file = 'notes.txt'
@@ -99,7 +107,7 @@ def read_notes() -> list[dict]:
     return note_list
 
 
-def init():
+def init_note_app():
     global note_list
     note_list = read_notes()
     max_note_id = find_top_note_id_functional(note_list)
@@ -114,30 +122,33 @@ def init():
 
 def main():
     while True:
-        command, *args = input("Please enter command (enter exit to stop): ").strip().split(
+        command, *args = input_colored("Please enter command (enter exit to stop): ").strip().split(
             ' ')  # Очистити пробіли (print_note 1)
         if command == 'exit':
             print("Goodbye!")
             save_notes()
             break
         elif command == 'add_note':
-            text = input("Please enter note text: ")
+            text = input_colored("Please enter note text: ")
             # add_new_note(text)
             if add_new_note(text):
-                print("\nNote added successfully!\n")
+                print_success("\nNote added successfully!\n")
             else:
-                print("\nError while adding a note!\n")
+                print_error("\nError while adding a note!\n")
         elif command == 'help':
             print(commands)
         elif command == "print_note":
+            if len(args) < 1:
+                print_error("Pleas enter a note number")
+                continue
             index = int(args[0]) - 1
             if index < 0 or index >= len(note_list):
-                print("Pleas enter a valid note number")
+                print_error("Pleas enter a valid note number")
                 continue
             print_note(index)
         elif command == 'print_all':
             print_all_notes()
 
 
-init()
+init_note_app()
 main()
