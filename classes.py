2e5464599f066d240a2b9f405f26e370718a3801
RuslanaPashkutska <ryslana.pashkytska@gmail.com>
2025-03-01 00:28:05 +0100

    users functionality

---
+++ classes.py
@@ -0,0 +1,6 @@
+#{"login": "user_one", "notes_ids": [1, 2, 3, 6], "first_name"}
+
+class NoteUser:
+    def __init__(self, login):
+        self.login = login
+        self.notes_ids = []
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from id_generator import id_generator
 
 from printer_functions import print_error, print_success, input_colored
-
+from classes import NoteUser
 from colorama import init
 
 
@@ -36,9 +36,10 @@ if note_date_one ["creation_date"] > note_date_one ["creation_date"]:
 '''
 init(autoreset=True)
 
-note_list = [] # {"creation_date": "13.02.2025 21:03", "text": "This is my note, that I am taking on my laptop", id: 1}
+note_list = [] # {"creation_date": "13.02.2025 21:03", "text": "This is my note, that I am taking on my laptop", "id": 1}
 note_file = 'notes.txt'
 note_id_generator = id_generator()
+user_list = []
 
 # Hello note;13.02.2025 21:03
 welcome_banner = """
@@ -57,20 +58,24 @@ commands = """
 """
 
 
-def add_new_note(note_text) -> bool:
+def add_new_note(note_text, current_user: NoteUser) -> bool:
     note_creation_date = datetime.today()
     next_id = note_id_generator()
     note_list.append({"text": note_text, "creation_date": note_creation_date, "id": next_id})
+    current_user.notes_ids.append(next_id)
     return True
 
-def print_note(index: int):
+def print_note(index: int, current_user: NoteUser):
     note = note_list[index]
+    if note['id'] not in current_user.notes_ids:
+        print_error("User can no access user this note")
+        return
     formatted_creation_date = note["creation_date"].strftime("%d.%m.%Y %H:%M")
     print(f'{note["id"]}: "{note["text"]}"\n- Created on {formatted_creation_date}\n')
 
-def print_all_notes():
+def print_all_notes(current_user: NoteUser):
     for note_index in range(len(note_list)):
-        print_note(note_index)
+        print_note(note_index, current_user)
 
 def find_top_note_id(notes: list[dict]) -> int:
     max_id = 0
@@ -120,7 +125,27 @@ def init_note_app():
     print(commands)
     print()
 
+def find_user_by_login(login: str) -> NoteUser | None:
+    note_user = None
+    for user in user_list:
+        if user.login == login:
+            note_user = user
+    return note_user
+
+
+def get_user() -> NoteUser:
+    user_login = input_colored("Please enter your username: ")
+    found_user = find_user_by_login(user_login)
+    if not found_user:
+        print_error("User not found")
+        found_user = NoteUser(user_login)
+        user_list.append(found_user)
+        print_success("User created")
+    return found_user
+
+
 def main():
+    current_user = get_user()
     while True:
         command, *args = input_colored("Please enter command (enter exit to stop): ").strip().split(
             ' ')  # Очистити пробіли (print_note 1)
@@ -131,7 +156,7 @@ def main():
         elif command == 'add_note':
             text = input_colored("Please enter note text: ")
             # add_new_note(text)
-            if add_new_note(text):
+            if add_new_note(text, current_user):
                 print_success("\nNote added successfully!\n")
             else:
                 print_error("\nError while adding a note!\n")
@@ -145,10 +170,12 @@ def main():
             if index < 0 or index >= len(note_list):
                 print_error("Pleas enter a valid note number")
                 continue
-            print_note(index)
+            print_note(index, current_user)
         elif command == 'print_all':
-            print_all_notes()
+            print_all_notes(current_user)
 
 
 init_note_app()
 main()
+
+
